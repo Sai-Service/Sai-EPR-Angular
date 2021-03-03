c2b6e9414fa01cbd3be8c490aa8d08423c6b3652
JyotiKadam-3006 <jyotik@saiservice.com>
2021-03-03 15:04:56 +0530

    StockTransfer

--- src/app/transaction/transaction-routing.module.ts
+++ src/app/transaction/transaction-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoveOrderComponent } from './move-order/move-order.component';
 import { PaymentsComponent } from './payments/payments.component';
 import { PoInvoiceComponent } from './po-invoice/po-invoice.component';
+import { StockTransferComponent } from './stock-transfer/stock-transfer.component';
 import { TdsInvoiceComponent } from './tds-invoice/tds-invoice.component';
 
 const routes: Routes = [
   {path:'PoInvoice', component: PoInvoiceComponent},
   {path:'TdsInvoice',component:TdsInvoiceComponent},
   {path:'Payment',component:PaymentsComponent},
+  {path:'MoveOrder',component:MoveOrderComponent},
+  {path:'stockTransfer',component:StockTransferComponent},
   { path : '' ,redirectTo: 'Dashboard', pathMatch: 'full'} 
 ];
 
--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -7,9 +7,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxDateRangeModule } from 'ngx-daterange';
 import { TdsInvoiceComponent } from './tds-invoice/tds-invoice.component';
 import { PaymentsComponent } from './payments/payments.component';
+import { MoveOrderComponent } from './move-order/move-order.component';
+import { StockTransferComponent } from './stock-transfer/stock-transfer.component';
 
 @NgModule({
-  declarations: [PoInvoiceComponent, TdsInvoiceComponent, PaymentsComponent],
+  declarations: [PoInvoiceComponent, TdsInvoiceComponent, PaymentsComponent, MoveOrderComponent, StockTransferComponent],
   imports: [
     CommonModule,
     TransactionRoutingModule, ReactiveFormsModule,NgxDateRangeModule,
