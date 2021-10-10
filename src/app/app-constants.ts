bd6e79bab787e6fe7eb25f4d5b52ae422734db1b
JyotiKadam-3006 <jyotik@saiservice.com>
2021-10-10 17:28:47 +0530

    commi

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,11 +1,6 @@
 export class AppConstants {
-<<<<<<< HEAD
     // ServerUrl='http://saihorizon.com:8080/ErpReplica'
     public static get ServerUrl(): string { return "http://localhost:8081"; }
     // public static get ServerUrl(): string { return "http://saihorizon.com:8080/ErpReplica"; }
-=======
-  public static get ServerUrl(): string { return "http://localhost:8081"; }
- //public static get ServerUrl(): string { return "http://saihorizon.com:8080/ErpReplica"; }
->>>>>>> 7efe8ed98cafa73e271148a009991344307c5dab
 
 }
