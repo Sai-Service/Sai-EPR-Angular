3de312bc6c373d8429ba800a76044727e9311369
Vinita Rane <vinitar@saiservice.com>
2021-10-28 12:28:26 +0530

    supplier and inv changes

--- src/app/transaction/stock-transfer/stock-transfer.component.ts
+++ src/app/transaction/stock-transfer/stock-transfer.component.ts
@@ -464,7 +464,7 @@ transdata(val){
 EwayUpdate(){
  // const formValue:IStockTransfer = this.transdata(this.stockTranferForm.value);
  const formValue:IEway = this.stockTranferForm.value;
- debugger;
+//  debugger;
     this.service.UpdateStkEway(formValue).subscribe((res: any) => {
     if (res.code === 200) {
       alert('RECORD UPDATED SUCCESSFULLY');
@@ -675,7 +675,7 @@ deleteReserveLinewise(i)
     if(ShipmentNo!=undefined){
     this.currentOp='SEARCH';
     // alert('1'+ShipmentNo);
-    this.display = true;
+    this.display = false;
     this.stockTranferForm.get('ewayBill').disable();
       this.stockTranferForm.get('ewayBillDate').disable();
     // var shipNo = (this.stockTranferForm.get('ShipmentNo').value);
@@ -705,17 +705,18 @@ deleteReserveLinewise(i)
           })
         }
         var eway=this.stockTranferForm.get('ewayBill').value;
-        alert(eway);
+        // alert(eway);
         if(eway===''){
-          alert('In If');
+          // alert('In If');
+          this.displayUp=false;
           this.stockTranferForm.get('ewayBill').enable();
           this.stockTranferForm.get('ewayBillDate').enable();
-          this.displayUp=false;
+
         }
         this.currentOp='INSERT';
         this.displayOp=false;
         // (document.getElementById('btnUpdate') as HTMLInputElement).disabled = false;
-        this.displayUp=true;
+        // this.displayUp=true;
       }
       );
     }
