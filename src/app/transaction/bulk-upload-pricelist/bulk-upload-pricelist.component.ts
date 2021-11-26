ad478a9c984d04708c0083e90c9000b926625153
JyotiKadam-3006 <jyotik@esaiservice.com>
2021-11-26 12:28:16 +0530

    bulk upload price list

--- src/app/transaction/bulk-upload-pricelist/bulk-upload-pricelist.component.ts
+++ src/app/transaction/bulk-upload-pricelist/bulk-upload-pricelist.component.ts
@@ -301,4 +301,13 @@ progressBarTesting(){
 }
 
 // ------------------------------------------------------
+
+exportToExcel1() {
+  const ws: xlsx.WorkSheet =   
+  xlsx.utils.table_to_sheet(this.epltable1.nativeElement);
+  const wb: xlsx.WorkBook = xlsx.utils.book_new();
+  xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
+  xlsx.writeFile(wb, 'epltable1.xlsx');
+ }
+
 }
