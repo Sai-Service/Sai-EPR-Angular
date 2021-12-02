b260b0008f99ea61631719e2a3e34f68313d821f
rkpr <rkpr@saiservice.com>
2021-12-02 17:55:18 +0530

    PL CHANGES & CUST REL ATION MASTER

---
+++ src/app/master/customer-relation-master/customer-relation-master.component.ts
@@ -0,0 +1,131 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Validators , FormArray } from '@angular/forms';
+import { MasterService } from '../master.service';
+import { NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ThemeService } from 'ng2-charts';
+import { DatePipe } from '@angular/common';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+
+interface ICustRelMaster { }
+
+@Component({
+  selector: 'app-customer-relation-master',
+  templateUrl: './customer-relation-master.component.html',
+  styleUrls: ['./customer-relation-master.component.css']
+})
+
+export class CustomerRelationMasterComponent implements OnInit {
+    custRelationMasterForm: FormGroup;
+
+    pipe = new DatePipe('en-US');
+    now = Date.now();
+
+    
+
+    public PriceListIdList : Array<string> = [];
+    priceListLineDetails : any=[];
+    priceListLineDetails1 : any=[];
+
+    loginName:string;
+    loginArray:string;
+    name:string;
+    ouName : string;
+    ouId:number;
+    locId: number;
+    deptId:number;
+    locName : string;
+    emplId :number;
+    orgId:number;
+    divisionId : number;
+    divisionName:string;
+
+    empTktNo:string;
+    custName:string;
+    emplName:string;
+    empDesig:string;
+    empStatus:string ='Active';
+    startDate = this.pipe.transform(Date.now(), 'y-MM-dd');
+    endDate:Date;
+
+  constructor(private service: MasterService, private fb: FormBuilder, private router: Router) 
+    {
+      this.custRelationMasterForm = fb.group({
+
+      loginArray:[''],
+      loginName:[''],
+      ouName :[''],
+      ouId:[''],
+      locId:[''],
+      locName :[''],
+      emplId:[''],
+      divisionId :[''],
+      divisionName:[''],
+
+      empTktNo:[],
+      custName:[],
+
+      emplName:[],
+      empDesig:[],
+      empStatus:[],
+      startDate:[],
+      endDate:[],
+
+      custList: this.fb.array([this.lineDetailsGroup()])   
+
+      });
+    }
+
+    lineDetailsGroup() {
+      return this.fb.group({ 
+        customerId:[''],
+        custAccountNo :['', [Validators.required]],    
+        custName:['', [Validators.required]],
+        custAddr: ['', [Validators.required]],
+        custMobile: ['', [Validators.required]],
+        startDate: ['', [Validators.required]],
+        endDate: ['', [Validators.required]],
+       });
+    }
+  
+   lineDetailsArray() :FormArray{
+      return <FormArray>this.custRelationMasterForm.get('custList')
+    }
+   
+
+  get f() { return this.custRelationMasterForm.controls; }
+  custRelationMaster(custRelationMasterForm:any) {  }
+
+  ngOnInit(): void {
+
+    this.name=  sessionStorage.getItem('name');
+    this.loginArray=sessionStorage.getItem('divisionName');
+    this.divisionName=sessionStorage.getItem('divisionName');
+    this.divisionId=Number(sessionStorage.getItem('divisionId'));
+    this.loginName=sessionStorage.getItem('name');
+    this.ouName = (sessionStorage.getItem('ouName'));
+    this.ouId=Number(sessionStorage.getItem('ouId'));
+    this.locId=Number(sessionStorage.getItem('locId'));
+    this.locName=(sessionStorage.getItem('locName'));
+    this.deptId=Number(sessionStorage.getItem('dept'));
+    this.emplId= Number(sessionStorage.getItem('emplId'));
+   
+    this.orgId=this.ouId;
+    console.log(this.loginArray);
+    console.log(this.locId);
+
+  }
+
+  SearchByEmpTktNo (tktNo){
+    alert("Search Employee...TktNo :"+tktNo);
+  }
+
+
+  SearchByCust (cust){
+    alert("Search Cust...TktNo :"+cust);
+  }
+  
+
+}
