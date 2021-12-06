dcfbfc5e5aff1aead45dda20d6a1ae7553aa5454
Vinita Rane <vinitar@saiservice.com>
2021-12-06 13:11:19 +0530

    customer changes

--- src/app/master/customer-master/customer-master.component.ts
+++ src/app/master/customer-master/customer-master.component.ts
@@ -741,6 +741,8 @@ export class CustomerMasterComponent implements OnInit {
         alert(res.message);
         var acctNo = this.customerMasterForm.get('custAccountNo').value;
         this.searchByAccount1(acctNo);
+        // this.customerMasterForm.disable();
+        this.displayNewButton1=false;
         // this.customerMasterForm.reset();
       } else {
         if (res.code === 400) {
