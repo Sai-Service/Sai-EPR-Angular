ce0b4b30e7870dc46de14ccb5c1d4e9b2ae52a94
JyotiKadam-3006 <jyotik@esaiservice.com>
2022-01-03 10:24:23 +0530

    counter performa inv

---
+++ src/app/order-management/counter-sale-performa-inv/counter-sale-performa-inv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-sale-performa-inv',
+  templateUrl: './counter-sale-performa-inv.component.html',
+  styleUrls: ['./counter-sale-performa-inv.component.css']
+})
+export class CounterSalePerformaInvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
