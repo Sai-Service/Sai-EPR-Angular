2282c406f93c8cf0a1f71dc9bacf17f3aa832f62
JyotiKadam-3006 <jyotik@esaiservice.com>
2022-01-15 15:22:33 +0530

    Sale & Service rervice reports added

--- src/app/report/reports-routing.module.ts
+++ src/app/report/reports-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AllReportsComponent } from './all-reports.component';
+import { SalesReportsComponent } from './sales-reports/sales-reports.component';
+import { ServiceReportComponent } from './service-report/service-report.component';
 
 
 
 const routes: Routes = [
   {path:'SparesReports',component:AllReportsComponent},
+  {path:'ServiceReport',component:ServiceReportComponent},
+  {path:'SalesReports',component:SalesReportsComponent},
   { path : '' ,redirectTo: 'Dashboard', pathMatch: 'full' }
 ];
 
