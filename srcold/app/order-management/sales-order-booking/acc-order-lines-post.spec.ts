dd616b57d6f21c0ad6451197f2be97f156c3fefc
rkpr6290 <rkpr917@gmail.com>
2022-01-17 17:54:23 +0530

    cashbank trf

--- srcold/app/order-management/sales-order-booking/acc-order-lines-post.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AccOrderLinesPost } from './acc-order-lines-post';
-
-describe('AccOrderLinesPost', () => {
-  it('should create an instance', () => {
-    expect(new AccOrderLinesPost()).toBeTruthy();
-  });
-});
