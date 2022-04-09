f167e5250ac8ab343a59b3e4b97fbf18d216a331
vinitar <vinitar@saiservice.com>
2022-04-09 12:48:37 +0530

    ar changes

--- src/app/transaction/arinvoice/arinvoice.component.ts
+++ src/app/transaction/arinvoice/arinvoice.component.ts
@@ -106,7 +106,7 @@ export class ARInvoiceComponent implements OnInit {
   // glDate:string;
   taxAmount: number;
   billToCustName: string;
-
+  displaytaxName:boolean=true;
   public applyTo = 'INVOICE'
   // now = Date.now();
   // new=new Date();
@@ -869,6 +869,7 @@ export class ARInvoiceComponent implements OnInit {
               this.displayRemoveRow = false;
               this.displayRmDistRow = false;
               this.disabledComplete = false;
+              this.displaytaxName=false;
               this.arInvoiceForm.disable();
             }
             if(data.obj.invStatus == 'Complete'){
