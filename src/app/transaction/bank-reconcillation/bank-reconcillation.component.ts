64b0a4c600b1f814644fa83c865cbb477399aae7
rkpr6290 <rkpr917@gmail.com>
2022-05-03 13:26:05 +0530

    bank recon

--- src/app/transaction/bank-reconcillation/bank-reconcillation.component.ts
+++ src/app/transaction/bank-reconcillation/bank-reconcillation.component.ts
@@ -25,6 +25,7 @@ interface IBankRecon {
 export class BankReconcillationComponent implements OnInit {
     bankReconcillationForm : FormGroup;
   
+        spinIcon=false;
         importButton=true;
         dataDisplay: any;
         msg:any;
@@ -634,6 +635,7 @@ export class BankReconcillationComponent implements OnInit {
         if (this.fileValidation===true) {
           this.updStatus=true; 
           this.importButton=false;
+          this.spinIcon=true;
           // this.progress = 0;
 
         this.dataDisplay ='File Upload in progress....Do not refresh the Page'
@@ -646,22 +648,25 @@ export class BankReconcillationComponent implements OnInit {
         formData.append('file', event)
            this.service.UploadExcelBankStatement(formData,this.docType,this.ouId,bnkAcId,stNum).subscribe((res: any) => {
             if (res.code === 200) {
-                this.resMsg = res.message+",  Code : "+res.code;;
-              //  this.lstMessage=res.obj.priceListDetailList;
-                this.dataDisplay ='File Uploaded Sucessfully....'
-               this.importButton=false;
+               this.resMsg = res.message+",  Code : "+res.code;;
+              //this.lstMessage=res.obj.priceListDetailList;
+               this.dataDisplay ='File Uploaded Sucessfully....'
+              //  this.importButton=false;
                this.viewLogFile=true;
+               this.spinIcon=false;
             } else {
               // if (res.code === 400) {
                  this.resMsg = res.message +",  Code : "+res.code;
                 // this.lstMessage=res.obj.priceListDetailList;
                 this.updStatus=false;
                 this.dataDisplay ='File Uploading Failed....'
-                this.importButton=false;
+                this.importButton=true;
+                this.spinIcon=false;
                 // this.viewLogFile=false;
               // }
             } });
-         } }
+         } else { alert ( "Please Select Upload File ...");}
+        }
 
 
 
