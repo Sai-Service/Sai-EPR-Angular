42c85326c24b6035fff1ba10cf8d39b9078430dd
JYOTIKADAM-3006 <jyotik@saiservice.com>
2023-06-02 15:15:57 +0530

    remove unwanted code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 // import { CanDeactivateGuard } from './can-deactivate-guard.service';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // {path: '', redirectTo: '', pathMatch: 'full'},
-  // {path: '**', component:PageNotFouundComponent},
-  // { path: 'admin', loadChildren: () => import('../app/body-main/body-main.module').then(m => m.BodyMainModule) },
   { path: 'admin', loadChildren: () => import('../app/admin/admin.module').then(m => m.AdminModule) },
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // {path: '', redirectTo: '', pathMatch: 'full'},
-  // {path: '**', component:PageNotFouundComponent},
-  // {path:'AdminSideBare',component:AdminSideBareComponent}
   {path :'login',loadChildren:()=> import('./auth/auth.module').then(m=> m.AuthModule)},
   { path : '' ,redirectTo: 'login', pathMatch: 'full' }
 ];
