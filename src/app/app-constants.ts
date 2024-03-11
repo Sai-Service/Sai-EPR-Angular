be1326f84dd8a41016a7b5c192a978428904384a
“JyotiKadam-3006” <jyotik@saiservice.com>
2024-03-11 12:03:59 +0530

    api change

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,7 +1,8 @@
 export class AppConstants {
      public static get ServerUrl(): string { return "http://localhost:8081"; }
 // public static get ServerUrl(): string { return "http://saihorizon.com:8051/ErpReplica"; }
+// public static get ServerUrl(): string { return "http://saierp.horizon.org:8080/ErpReplica"; }
     // public static get ServerUrl(): string { return "http://saierp.horizon.org:8080/PetrolPump"; }
- public static get ServerUrl(): string { return "http://saierp.horizon.org:7867/PetrolPump"; }
+//  public static get ServerUrl(): string { return "http://saierp.horizon.org:7867/PetrolPump"; }
    
 }
--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -26,8 +26,9 @@ export class LoginService {
     };
     const url = 'http://localhost:8081/loginpage';
     //  const url='http://saihorizon.com:8051/ErpReplica/loginpage';
+    //  const url='http://saierp.horizon.org:8080/ErpReplica/loginpage';
     // const url= "http://saierp.horizon.org:8080/PetrolPump/loginpage"; 
-    const url= "http://saierp.horizon.org:7867/PetrolPump/loginpage"; 
+    // const url= "http://saierp.horizon.org:7867/PetrolPump/loginpage"; 
   
     console.log(body);
     return this.httpclient.post(url, body, options)
