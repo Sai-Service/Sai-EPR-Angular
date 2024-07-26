dbf668498bef084543351e8532b70b611952c27d
“JyotiKadam-3006” <jyotik@saiservice.com>
2024-07-26 11:30:41 +0530

    counter sale changes done

--- src/app/order-management/counter-sale/counter-sale.component.ts
+++ src/app/order-management/counter-sale/counter-sale.component.ts
@@ -1889,7 +1889,7 @@ export class CounterSaleComponent implements OnInit, OnDestroy {
 
   onKey(index, fldName, event) {
     // alert (event.keyCode)
-    alert("index, fldName, event >> "+index+ ","+ fldName +","+ event.keyCode);
+    // alert("index, fldName, event >> "+index+ ","+ fldName +","+ event.keyCode);
     if (event.keyCode != 13) {
       // alert(index +'Onkey Alert' +'---'+fldName)
       // var arrayControl = this.CounterSaleOrderBookingForm.get('oeOrderLinesAllList').value;
