fd791f949289f5544f314b4edc8946c37020fc35
“JyotiKadam-3006” <jyotik@saiservice.com>
2025-01-10 18:23:21 +0530

    code

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,9 +1,5 @@
 export class AppConstants {
-<<<<<<< HEAD
      // public static get ServerUrl(): string { return "http://localhost:8081"; }
-=======
-     public static get ServerUrl(): string { return "http://localhost:8081"; }
->>>>>>> b64b9de60ff869483251a4903c0489bb9425f06a
 // public static get ServerUrl(): string { return "http://saihorizon.com:8051/ErpReplica"; }
 // public static get ServerUrl(): string { return "http://saierp.horizon.org:8080/ErpReplica"; }
   
