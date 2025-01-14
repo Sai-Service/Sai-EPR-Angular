738f4aa5b5d78c599d2a6d3bdd22e3d452845081
rkpr6290 <rkpr@saiservice.com>
2025-01-14 12:52:21 +0530

    rkpnt-2

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,11 +1,6 @@
 export class AppConstants {
-<<<<<<< HEAD
      public static get ServerUrl(): string { return "http://localhost:8081"; }
 // public static get ServerUrl(): string { return "http://saihorizon.com:8051/ErpReplica"; }
-=======
-     // public static get ServerUrl(): string { return "http://localhost:8081"; }
-public static get ServerUrl(): string { return "http://saihorizon.com:8051/ErpReplica"; }
->>>>>>> a4978284200ff71c5ef55543beb194ffb9647c4d
 // public static get ServerUrl(): string { return "http://saierp.horizon.org:8080/ErpReplica"; }
   
 //  public static get ServerUrl(): string { return "http://saierp.horizon.org:7867/PetrolPump"; }
