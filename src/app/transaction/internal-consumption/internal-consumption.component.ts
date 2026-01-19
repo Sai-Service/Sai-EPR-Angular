014c857438102f8da4af70f93f88306175b1c093
rkpr6290 <rkpr@saiservice.com>
2026-01-19 13:09:51 +0530

    rk-bajaj-ic-190126

--- src/app/transaction/internal-consumption/internal-consumption.component.ts
+++ src/app/transaction/internal-consumption/internal-consumption.component.ts
@@ -535,7 +535,7 @@ export class InternalConsumptionComponent implements OnInit {
         var op = valOp.split('-');
         var value1 = op[1];
 
-        // alert(reasonArray[2]);
+        alert(reasonArray[2]);
         // if(reasonArray[2]!=undefined){
         if (reasonArray[2].includes('OEM') && value1 == 'MRP') {
           this.service.getCostDetailforWarranty(Number(sessionStorage.getItem('locId')), select1.itemId).subscribe
@@ -547,6 +547,7 @@ export class InternalConsumptionComponent implements OnInit {
               }
             });
         }
+
         else if (reasonArray[2].includes('OEM') && value1 == 'NDP') {
           this.service.getCostDetail(Number(sessionStorage.getItem('locId')), select1.itemId).subscribe
             (data => {
@@ -557,6 +558,18 @@ export class InternalConsumptionComponent implements OnInit {
               }
             });
         }
+
+         else if (reasonArray[2].includes('IC51') && value1 == 'NDP') {
+          this.service.getCostDetail(Number(sessionStorage.getItem('locId')), select1.itemId).subscribe
+            (data => {
+              this.CostDetail = data;
+              trxLnArr1.controls[i].patchValue({ itemUnitCost: this.CostDetail.rate });
+              if (this.CostDetail.rate === 0.0) {
+                alert(this.CostDetail.segment);
+              }
+            });
+        }
+
         else if (reasonArray[2].includes('EW')) {
           this.service.getCostDetailforWarranty(Number(sessionStorage.getItem('locId')), select1.itemId).subscribe
             (data => {
